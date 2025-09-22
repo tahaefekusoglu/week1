1d94ae9f9084b40de3a0d6ea6711ce3288aae127
tahaefekusoglu <tahaefekusoglu@gmail.com>
2025-09-22 14:50:26 +0300

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,30 @@
+class plane :
+    def __init__(self,id,position):
+        self.position=(0,0,0)
+        self.id=id
+
+        def begin_fly(self,position):
+            self.position=position
+            def end_fly(self,position):
+                self.position=position
+                def go_forward(self,position):
+                 self.position=position
+
+                def  show(self):
+                 print('plane{},coordinates on {} now'.format ('args:self.id.self.position'))
+
+            uçak1=plane(id,'u1',(0,0,0))
+
+
+class filo:
+    def __init__(self, id, position):
+        self.planes=[]
+    def show(self):
+          for i in self.planes:
+              print('our filo include {}'.format(i.id))
+    def add_filo(self,plane):
+                  self.planes.append(plane)
+
+
+
+filo1=filo()
